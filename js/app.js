782687025dced1be17ac36cfa451469236445c32
Hedwige Uhllanda <hedwigeuhllanda@Hedwiges-MacBook-Air.local>
2022-03-16 21:21:02 -0600

    Started Javascript

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var userAge = 18;
+console.log(userAge);
+
+var username = "subscribed";
+
+//var bool = true;
+//console.log(typeof(bool));
+
+if (userAge >= 18){
+    console.log("user is old enough");
+}else if (userAge < 18){
+    console.log("user is not old enough");
+}if (username == "subscribed"){
+    console.log("This user is subscribed");
+}else {
+    console.log("This user is not subscribed");
+}
